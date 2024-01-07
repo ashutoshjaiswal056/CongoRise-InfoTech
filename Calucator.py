8d18d3ffbded283f1cf6628c141bf281366c2d34
Ashutosh jaiswal <155469348+ashutoshjaiswal056@users.noreply.github.com>
2024-01-07 09:34:21 -0800

    Add files via upload

---
+++ Calucator.py
@@ -0,0 +1,23 @@
+# MINI PROJECT
+# CREATED BY ASHUTOSH JAISWAL
+first= input("enter first number")
+operator= input("enter operator(+,-,*,/,%):")
+second=input("enter second number")
+first= int(first)
+second=int(second)
+
+if operator =="+":
+   print(first+second)
+elif operator =="-":
+   print(first-second)
+elif operator =="*":
+   print(first*second)
+elif operator =="/":
+   print(first/second)
+elif operator =="%":
+   print(first % second)
+else:
+    print("invalid operator")
+   
+
+
